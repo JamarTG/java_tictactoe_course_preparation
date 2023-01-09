9e17b52fc936fc5eed8398a76bfa2a65edbc95fd
JamariTheGreat <jamarimcfarlane12@gmail.com>
2023-01-09 18:45:14 -0500

    removed unneceessary files

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+
+
+
 import java.util.Scanner;
 import java.lang.Process;
 import java.util.InputMismatchException;
@@ -6,115 +9,101 @@ import java.io.Console;
 
 class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
 
-	  System.out.println("""
+        System.out.println("""
+// ████████╗██╗░█████╗░████████╗░█████╗░░█████╗░████████╗░█████╗░███████╗
+// ╚══██╔══╝██║██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔════╝
+// ░░░██║░░░██║██║░░╚═╝░░░██║░░░███████║██║░░╚═╝░░░██║░░░██║░░██║█████╗░░
+// ░░░██║░░░██║██║░░██╗░░░██║░░░██╔══██║██║░░██╗░░░██║░░░██║░░██║██╔══╝░░
+// ░░░██║░░░██║╚█████╔╝░░░██║░░░██║░░██║╚█████╔╝░░░██║░░░╚█████╔╝███████╗
+// ░░░╚═╝░░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░░╚════╝░╚══════╝
+        """);
+            Scanner scanner = new Scanner(System.in); System.out.print("Press the 'Enter' key to continue ..."); scanner.nextLine();
 
-░█████╗░░█████╗░██╗░░░░░██╗██╗░░░██╗░█████╗░██╗░░██╗██╗░██████╗
-██╔══██╗██╔══██╗██║░░░░░██║╚██╗░██╔╝██╔══██╗██║░░██║╚█║██╔════╝
-███████║███████║██║░░░░░██║░╚████╔╝░███████║███████║░╚╝╚█████╗░
-██╔══██║██╔══██║██║░░░░░██║░░╚██╔╝░░██╔══██║██╔══██║░░░░╚═══██╗
-██║░░██║██║░░██║███████╗██║░░░██║░░░██║░░██║██║░░██║░░░██████╔╝
-╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═════╝░
 
-████████╗██╗░█████╗░████████╗░█████╗░░█████╗░████████╗░█████╗░███████╗
-╚══██╔══╝██║██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔════╝
-░░░██║░░░██║██║░░╚═╝░░░██║░░░███████║██║░░╚═╝░░░██║░░░██║░░██║█████╗░░
-░░░██║░░░██║██║░░██╗░░░██║░░░██╔══██║██║░░██╗░░░██║░░░██║░░██║██╔══╝░░
-░░░██║░░░██║╚█████╔╝░░░██║░░░██║░░██║╚█████╔╝░░░██║░░░╚█████╔╝███████╗
-░░░╚═╝░░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░░╚════╝░╚══════╝""");
-  Scanner scanner = new Scanner(System.in);
-	System.out.print("Press the 'Enter' key to continue ...");
-  	scanner.nextLine();
-	  
-	  
-	  System.out.print("\033[H\033[2J");
-	  System.out.flush();
-	
-	  
+            System.out.print("\033[H\033[2J"); System.out.flush();
 
-    // determine which player is to move
-    boolean playerOneTurn = true;
 
-    // creates a board object
-    Board board = new Board();
 
-    // Set Name
-    System.out.println("Enter the name of player 1: ");
-    String playerOneName = scanner.next();
+            // determine which player is to move
+            boolean playerOneTurn = true;
 
-    System.out.println("Enter the name of player 2: ");
-    Scanner p2NameScanner = new Scanner(System.in);
-    String playerTwoName = p2NameScanner.next();
+            // creates a board object
+            Board board = new Board();
 
-    // creates the player
-    Player playerOne = new Player(playerOneName, 'X');
-    Player playerTwo = new Player(playerTwoName, 'O');
-    // This will continue to run the game until there is a winner or no empty spaces
-    while (board.evaluateBoard() == '-') {
+            // Set Name
+            System.out.println("Enter the name of player 1: "); String playerOneName = scanner.next();
 
-	  			System.out.print("\033[H\033[2J");
-	  System.out.flush();
+            System.out.println("Enter the name of player 2: "); Scanner p2NameScanner = new Scanner(System.in); String playerTwoName = p2NameScanner.next();
 
-      
-      Player currentPlayer;
-      board.drawBoard();
+            // creates the player
+            Player playerOne = new Player(playerOneName, 'X'); Player playerTwo = new Player(playerTwoName, 'O');
+            // This will continue to run the game until there is a winner or no empty spaces
+            while (board.evaluateBoard() == '-') {
 
-      if (playerOneTurn) {
-        currentPlayer = playerOne;
-      } else {
-        currentPlayer = playerTwo;
-      }
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
 
-	  System.out.println(currentPlayer.getName() + " is playing " + currentPlayer.getPlayerLetter());
 
-      boolean valid = false;
-      int move = 0;
+                Player currentPlayer;
+                board.drawBoard();
 
-      do {
-        try {
-          move = scanner.nextInt();
-		  
-          valid = true;
-        } catch (InputMismatchException e) {
-          System.out.println("This is not a valid move");
-          scanner.nextLine();
-          continue;
+                if (playerOneTurn) {
+                    currentPlayer = playerOne;
+                } else {
+                    currentPlayer = playerTwo;
+                }
+
+                System.out.println(currentPlayer.getName() + " is playing " + currentPlayer.getPlayerLetter());
+
+                boolean valid = false;
+                int move = 0;
+
+                do {
+                    try {
+                        move = scanner.nextInt();
+
+                        valid = true;
+                    } catch (InputMismatchException e) {
+                        System.out.println("This is not a valid move");
+                        scanner.nextLine();
+                        continue;
+                    }
+
+                    if (!(move > 0 && move < 10) || board.isPlayed(move)) {
+                        System.out.println("That is an invalid move.");
+                        valid = false;
+                        continue;
+                    }
+                } while (!valid);
+
+
+                board.addMove(move);
+                currentPlayer.setMove(move);
+                board.updateBoard(currentPlayer);
+
+                playerOneTurn = !playerOneTurn;
+
+                switch (board.evaluateBoard()) {
+                    case 'D':
+                        System.out.print("The game is a draw!");
+                        break;
+                    case '-':
+                        break;
+                    default:
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+
+                        board.drawBoard();
+                        System.out.print("The winner is " + currentPlayer.getName());
+
+
+
+
+                        break;
+                }
+            }
         }
-
-        if (!(move > 0 && move < 10) || board.isPlayed(move)) {
-          System.out.println("That is an invalid move.");
-		  valid = false;
-          continue;
-        }
-      } while (!valid);
-      
-      
-      board.addMove(move);
-      currentPlayer.setMove(move);
-      board.updateBoard(currentPlayer);
-
-      playerOneTurn = !playerOneTurn;
-
-      switch (board.evaluateBoard()) {
-        case 'D':
-          System.out.print("The game is a draw!");
-          break;
-        case '-':
-          break;
-        default:	  
-		  System.out.print("\033[H\033[2J");
-	      System.out.flush();
-          
-		  board.drawBoard();
-          System.out.print("The winner is " + currentPlayer.getName());
-
-
-
-          
- 		  break;
-	  }
     }
-  }
-}
